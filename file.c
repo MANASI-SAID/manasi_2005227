892a2947bf1d32345ad50b6d4b7ecb2c44db8269
MANASI-SAID <66167165+MANASI-SAID@users.noreply.github.com>
2020-06-18 10:06:24 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int x,y;
+   printf("Enter x: \n");
+   scanf("%d",&x);
+   printf("Enter y: \n");
+   scanf("%d",&y);
+
+   if(x>0 && y>0)
+    printf("1st Quadrant");
+   else if(x>0 && y<0)
+    printf("2nd quadrant");
+   else if(x<0 && y<0)
+    printf("3rd Quadrant");
+   else if("x<0 && y>0")
+    printf("4th Quadrant");
+
+}
